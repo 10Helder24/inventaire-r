37050318fb33abfd349a9646ee2db6576761ad21
10Helder24 <ferreirahelder.m@gmail.com>
2025-04-09 08:45:43 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,14 @@ function App() {
     loadArticles();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <div className="text-gray-500">Chargement...</div>
+      </div>
+    );
+  }
+
   return (
     <Router>
       <Routes>
